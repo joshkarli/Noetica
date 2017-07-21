e6a0910211ebfaf4bb0e65806e7afac6a1a4d4eb
Josh Karli <josh.karli@gmail.com>
2017-07-20 21:39:38 -0700

    syncing with master

--- Hydraulics/hydraulics.py
+++ Hydraulics/hydraulics.py
@@ -14,36 +14,42 @@ import hydraulics_stream
 HTTP_PORT = 9000 
 POSITION_PORT = 9001
 HYDRAULICS_POLL_INTERVAL = 200  # hardware poll interval, in ms
+PLAYBACK_DIR = "/home/flaming/Noetica/Hydraulics/playbacks/"
 
 
-logging.basicConfig(format='%(asctime)-15s')
-
+logging.basicConfig(format='%(asctime)-15s %(levelname)s %(message)s', level=logging.DEBUG)
+#logging.basicConfig(format='%(asctime)-15s', level=logging.DEBUG)
+#logging.basicConfig(format=level=logging.DEBUG)
 if __name__ == '__main__':
 
-    log.info("Noetica Hydraulics")
-    if len(sys.argv > 1):
+    logging.info("Noetica Hydraulics")
+    print "STARTING NOETICA"
+    if len(sys.argv) > 1:
         configFile = sys.argv[1]
     else:
-        configFile = "./hydraulics.cfg"
+        configFile = "./noetica.cfg"
+        
+    httpd = None
     
     try:
         configParser = ConfigParser.ConfigParser()   
         configParser.read(configFile)
-        HTTP_PORT     = configParser.get("main", "httpPort", HTTP_PORT)
-        POSITION_PORT = configParser.get("main", "streamPort", POSITION_PORT)
-        HYDRAULICS_POLL_INTERVAL = configParser.get("main", "pollInterval", HYDRAULICS_POLL_INTERVAL)
-    except:
+        HTTP_PORT     = int(configParser.get("hydraulics", "httpPort", HTTP_PORT))
+        POSITION_PORT = int(configParser.get("hydraulics", "streamPort", POSITION_PORT))
+        HYDRAULICS_POLL_INTERVAL = int(configParser.get("hydraulics", "pollInterval", HYDRAULICS_POLL_INTERVAL))
+        PLAYBACK_DIR = configParser.get("hydraulics", "playbackDir", PLAYBACK_DIR)
+    except Exception:
         logging.error("Problem reading config file {}, defaulting configuration".format(configFile))
 
     try:
+        # setup position streamer - XXX should be in own process
+        hydraulics_stream.init(POSITION_PORT)
+
         # setup driver  - XXX own process?
         hydraulics_drv.init(HYDRAULICS_POLL_INTERVAL)
 
         # setup playback - NB - this is a set of utility functions, not own thread
-        hydraulics_playback.init()
-        
-        # setup position streamer - XXX should be in own process
-        hydraulics_stream.init(POSITION_PORT)
+        hydraulics_playback.init()        
 
         # initialize httpserver 
         httpd = BaseHTTPServer.HTTPServer(("", HTTP_PORT), hydraulics_frontend.HydraulicsHandler)
@@ -53,10 +59,8 @@ if __name__ == '__main__':
     except Exception as e:
         logging.exception("Unexpected exception:")
     
-    try: 
+    if httpd != None:
         httpd.server_close()
-    except NameError:
-        print("Could not find httpd") # XXX wtf is this for?
-        
+
     hydraulics_drv.shutdown()
     hydraulics_stream.shutdown()
